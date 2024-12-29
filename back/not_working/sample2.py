dc7d2fd2d4c33b5a95d7c24ce6830a12cde7608f
gepuro <gepuro@gmail.com>
2024-12-29 16:55:22 +0900

    update

--- back/not_working/sample2.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="GEMINI_API_KEY")
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash-exp", contents="How does AI work?"
-)
-print(response.text)
